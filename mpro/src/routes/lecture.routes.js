17eec87f32a8e896c93a6a7def90b2233c2fa2dc
etnsum <144820152+etnsum@users.noreply.github.com>
2024-11-28 20:55:23 +0900

    Delete mpro directory

--- mpro/src/routes/lecture.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getRatingStats } from "../controllers/lecture.controller.js";
-
-const router = express.Router();
-
-router.get("/:lectureId/rating-stats", getRatingStats);
-
-export default router;
